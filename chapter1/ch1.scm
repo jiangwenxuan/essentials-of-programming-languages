800a5dcf7e54b32ac9f9d31c8b3271a1ec489842
jwx <904653684@qq.com>
2019-05-22 11:41:46 +0800

    ch1

--- chapter1/ch1.scm
+++ chapter1/ch1.scm
@@ -20,8 +20,58 @@
         (if (zero? n)
             (car lst)
             (nth-element (cdr lst) (- n 1))))))
-(define report-list-too-short
-  (lambda (n)
-    (eopl:error 'nth-element
-                "list too short by ~s elements.~%" (+ n 1))))
+;(define report-list-too-short
+;  (lambda (n)
+;    (eopl:error 'nth-element
+;                "list too short by ~s elements.~%" (+ n 1))))
+
+(define remove-first
+  (lambda (s los)
+    (if (null? los)
+        '()
+        (if (eqv? (car los) s)
+            (cdr los)
+            (cons (car los) (remove-first s (cdr los)))))))
+
+(define occurs-free?
+  (lambda (var exp)
+    (cond
+      ((symbol? exp) (eqv? var exp))
+      ((eqv? (car exp) 'lambda)
+       (and
+        (not (eqv? var (car (cadr exp))))
+        (occurs-free? var (caddr exp))))
+      (else
+       (or
+        (occurs-free? var (car exp))
+        (occurs-free? var (cadr exp)))))))
+
+(define subst
+  (lambda (new old slist)
+    (if (null? slist)
+        '()
+        (cons (subst-in-s-exp new old (car slist))
+              (subst new old (cdr slist))))))
+
+(define subst-in-s-exp
+  (lambda (new old sexp)
+    (if (symbol? sexp)
+        (if (eqv? sexp old)
+            new
+            sexp)
+        (subst new old sexp))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
