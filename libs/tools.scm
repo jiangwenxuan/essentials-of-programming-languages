96de8888f46f48740d54a12bd121243f6434f069
jwx <904653684@qq.com>
2019-05-24 17:21:00 +0800

    add libs

---
+++ libs/tools.scm
@@ -0,0 +1,14 @@
+#lang racket
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define append
+  (lambda (s1 s2)
+    (if (null? s1)
+        s2
+        (cons (car s1) (append (cdr s1) s2)))))
+
+
+(provide atom? append)
