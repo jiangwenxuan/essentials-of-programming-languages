49ed696aee5cc6aa9c5255e6086a105f1b64b228
jwx <904653684@qq.com>
2019-05-28 22:30:10 +0800

    use let to do 1.35

---
+++ chapter1/1.35-2.scm
@@ -0,0 +1,30 @@
+#lang racket
+
+(require "./1.31.scm")
+
+(define number-leaves
+  (lambda (btree)
+    (let ((n -1))
+      (define replace-leaf-with
+        (lambda (btree)
+          (cond
+            ((leaf? btree)
+             (begin
+               (set! n (+ n 1))
+               n))
+            (else
+             (interior-node (contents-of btree)
+                            (replace-leaf-with (lson btree))
+                            (replace-leaf-with (rson btree)))))))
+      (replace-leaf-with btree))))
+
+(define tree (interior-node 'foo
+                            (interior-node 'bar
+                                           (leaf 26)
+                                           (leaf 12))
+                            (interior-node 'baz
+                                           (leaf 11)
+                                           (interior-node 'quux
+                                                          (leaf 117)
+                                                          (leaf 14)))))
+(display (number-leaves tree))  
