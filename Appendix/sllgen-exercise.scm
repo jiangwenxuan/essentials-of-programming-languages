86bad43dce0a2d87f2b720f20938ad3868f126d1
jwx <904653684@qq.com>
2019-06-06 16:47:25 +0800

    learn a little about sllgen: scheme LL generator

--- Appendix/sllgen-exercise.scm
+++ Appendix/sllgen-exercise.scm
@@ -1,36 +1,95 @@
 #lang eopl
 
-(define-datatype statement statement?
-  [compound-statement
-   (stmt1 statement?)
-   (stmt2 statement?)]
-  [while-statement
-   (test expression?)
-   (body statement?)]
-  [assign-statement
-   (lhs symbol?)
-   (rhs expression?)])
+;(define-datatype statement statement?
+;  [compound-statement
+;   (compound-statements (list-of statement?))]
+;  [while-statement
+;   (test expression?)
+;   (body statement?)]
+;  [assign-statement
+;   (lhs symbol?)
+;   (rhs expression?)])
+;
+;(define-datatype expression expression?
+;  [var-exp
+;   (var symbol?)]
+;  [diff-exp
+;   (exp1 expression?)
+;   (exp2 expression?)])
+;
+;(define grammar-a1
+;  '((statement
+;     ("{" statement ";" statement "}")
+;     compound-statement)
+;    (statement
+;     ("while" expression "do" statement)
+;     while-statement)
+;    (statement
+;     (identifier ":=" expression)
+;     assign-statement)
+;    (expression
+;     (identifier)
+;     var-exp)
+;    (expression
+;     ("(" expression "-" expression ")")
+;     diff-exp)))
+
+(define scanner-spec-a
+  '((white-sp (whitespace) skip)
+    (commit ("%" (arbno (not #\newline))) skip)
+    (identifier (letter (arbno (or letter digit))) symbol)
+    (number (digit (arbno digit)) number)))
+
+(define list-of
+  (lambda (pred)
+    (lambda (x)
+      (or (null? x) 
+          (and (pair? x)
+               (pred (car x))
+               ((list-of pred) (cdr x)))))))
+
+;(define scan&parse2
+;  (sllgen:make-string-parser scanner-spec-a grammar-a1))
+;
+;(display (scan&parse2 "{x := foo; y := bar; z := uu;}"))
+
+(define grammar-a3
+  '((expression (identifier) var-exp)
+    (expression
+     ("let" (arbno identifier "=" expression) "in" expression)
+     let-exp)))
+
+(define scan&parse3
+  (sllgen:make-string-parser scanner-spec-a grammar-a3))
 
 (define-datatype expression expression?
-  [var-exp
-   (var symbol?)]
-  [diff-exp
-   (exp1 expression?)
-   (exp2 expression?)])
+  (var-exp (var-exp4 symbol?))
+  (let-exp (let-exp9 (list-of symbol?))
+           (let-exp7 (list-of expression?))
+           (let-exp8 expression?))
+  (num-exp (num number?)))
 
-(define grammar-a1
+(display (scan&parse3 "let x = y u = v in z"))
+
+(define grammar-a5
   '((statement
-     ("{" statement ";" statement "}")
+     ("{"
+      (separated-list
+       (separated-list identifier ",")
+       ":="
+       (separated-list expression ",")
+       ";")
+      "}")
      compound-statement)
-    (statement
-     ("while" expression "do" statement)
-     while-statement)
-    (statement
-     (identifier ":=" expression)
-     assign-statement)
-    (expression
-     (identifier)
-     var-exp)
-    (expression
-     ("(" expression "-" expression ")")
-     diff-exp)))
+    (expression (number) num-exp)
+    (expression (identifier) var-exp)))
+
+(define-datatype statement statement?
+  (compound-statement
+   (compound-statement4 (list-of (list-of symbol?)))
+   (compound-statement3 (list-of (list-of expression?)))))
+
+(define scan&parse5
+  (sllgen:make-string-parser scanner-spec-a grammar-a5))
+
+(display (scan&parse5 "{x, y := u, v; z := 4; t1, t2 := 5, 6}"))
